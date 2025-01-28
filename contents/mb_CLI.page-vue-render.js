3374d40aa1ab55faa4b1bf523b61ce857b6c2392
Ger Teck <changerteck@gmail.com>
2025-01-28 22:05:57 +0800

    Site Update. [skip ci]

---
+++ contents/mb_CLI.page-vue-render.js
@@ -0,0 +1,34 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"sticky":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/index.html","title":"Home"}},[_c('img',{attrs:{"src":"/assets/logo.png","height":"70"}})])]},proxy:true},{key:"right",fn:function(){return [_c('li',[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])})],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('overlay-source',{attrs:{"id":"site-nav","tag-name":"nav","to":"site-nav"}},[_c('div',{staticClass:"site-nav-top"},[_c('div',{staticClass:"fw-bold mb-2",staticStyle:{"font-size":"1.25rem"}},[_v("Contents")])]),_v(" "),_c('div',{staticClass:"nav-component slim-scroll"},[_c('site-nav',[_c('overlay-source',{staticClass:"site-nav-list site-nav-list-root",attrs:{"tag-name":"ul","to":"mb-site-nav"}},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/index.html"}},[_v("How does MarkBind Work 🏠")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/architecture.html"}},[_v("MarkBind Architecture")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/mb_cli.html"}},[_v("Command Line Interface")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic2.html"}},[_v("Topic 2")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-0",attrs:{"onclick":"handleSiteNavClick(this)"}},[_v("Topic 3 \n\n"),_c('div',{staticClass:"site-nav-dropdown-btn-container"},[_c('i',{staticClass:"site-nav-dropdown-btn-icon site-nav-rotate-icon",attrs:{"onclick":"handleSiteNavClick(this.parentNode.parentNode, false); event.stopPropagation();"}},[_c('span',{staticClass:"glyphicon glyphicon-menu-down",attrs:{"aria-hidden":"true"}})])])]),_c('ul',{staticClass:"site-nav-dropdown-container site-nav-dropdown-container-open site-nav-list"},[_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic3a.html"}},[_v("Topic 3a")])])]),_v(" "),_c('li',[_c('div',{staticClass:"site-nav-default-list-item site-nav-list-item-1",attrs:{"onclick":"handleSiteNavClick(this)"}},[_c('a',{attrs:{"href":"/contents/topic3b.html"}},[_v("Topic 3b")])])])])])])],1)],1)]),_v(" "),_c('div',{attrs:{"id":"content-wrapper"}},[_c('breadcrumb'),_v(" "),_m(0),_v(" "),_c('br'),_v(" "),_m(1),_v(" "),_c('pic',{attrs:{"src":"/diagrams/cli/markbind_cli.png","width":"900"}}),_v(" "),_m(2),_v(" "),_m(3),_v(" "),_m(4),_v(" "),_m(5),_v(" "),_m(6),_v(" "),_m(7),_v(" "),_c('panel',{scopedSlots:_u([{key:"header",fn:function(){return [_c('p',[_v("Activity Diagram - markbind init")])]},proxy:true}])},[_v(" "),_c('pic',{attrs:{"src":"/diagrams/cli/init.png","width":"900"}})],1)],1),_v(" "),_c('overlay-source',{attrs:{"id":"page-nav","tag-name":"nav","to":"page-nav"}},[_c('div',{staticClass:"nav-component slim-scroll"})]),_v(" "),_c('scroll-top-button')],1),_v(" "),_m(8)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{attrs:{"id":"markbind-command-line-interface"}},[_v("MarkBind Command Line Interface"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#markbind-command-line-interface","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("MarkBind makes use of the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("commander.js")]),_v(" npm package to build its CLI application, which simplifies the defining, parsing, of commands and adding options.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_c('br'),_v(" "),_c('br')])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("The command line supports a total of 4 commands. "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("init")]),_v(", "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("serve")]),_v(", "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("build")]),_v(" and "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("deploy")]),_v(".")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Each command is refactored into the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("cli/src/cmd")]),_v(" folder.")])}
+},function anonymous(
+) {
+with(this){return _c('h2',{attrs:{"id":"markbind-init"}},[_v("markbind Init"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#markbind-init","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("When we run "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("markbind init")]),_v(", we call the async function "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("init(root, options)")]),_v(". This function creates a new "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("Template")]),_v(" object, which either initalizes a new markbind project, or converts existing documentation into a markbind project.")])}
+},function anonymous(
+) {
+with(this){return _c('p',[_v("Here is an activity diagram depicting the "),_c('code',{pre:true,attrs:{"class":"hljs inline no-lang"}},[_v("init")]),_v(" function.")])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Generated by "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v("]")])])])}
+}];
+  
